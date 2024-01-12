9070864e169f53471d39fc3a4c71f03a903dfe83
Greywind299792458 <71220948+Greywind299792458@users.noreply.github.com>
2024-01-12 20:23:45 +0100

    ➕ added Material

--- frontend/time-travel-agency/src/app/app.module.ts
+++ frontend/time-travel-agency/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// todo: move all the material stuff in separate module
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
