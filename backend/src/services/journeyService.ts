906ca5c860e4131e8460a509bc374dd9050cfad9
bchollet <babacouda@gmail.com>
2024-01-27 14:22:00 +0100

    feat: added unit test with jest

--- backend/src/services/journeyService.ts
+++ backend/src/services/journeyService.ts
@@ -9,18 +9,18 @@ import {
 
 export async function getAllJourneys(): Promise<Journey[]> {
   return await findAllJourneys().then((daos: JourneyDAO[]): Journey[] => {
-    return daos.map((dao: JourneyDAO): Journey => toDTO(dao));
+    return daos.map((dao: JourneyDAO): Journey => toJourneyDTO(dao));
   });
 }
 
 export async function getJourneyById(id: number): Promise<Journey> {
   return await findJourneyById(id).then((dao: JourneyDAO): Journey => {
-    return toDTO(dao);
+    return toJourneyDTO(dao);
   });
 }
 
 export async function addJourney(journey: Journey) {
-  return await createJourney(toDAO(journey)).then((dao: JourneyDAO) => {
+  return await createJourney(toJourneyDAO(journey)).then((dao: JourneyDAO) => {
     console.info(`Journey with id ${dao.id} created`);
   });
 }
@@ -31,7 +31,7 @@ export async function delJourney(id: number) {
   });
 }
 
-function toDTO(dao: JourneyDAO): Journey {
+export function toJourneyDTO(dao: JourneyDAO): Journey {
   return {
     id: dao.id,
     startDate: dao.start_date,
@@ -71,7 +71,7 @@ function toDTO(dao: JourneyDAO): Journey {
   };
 }
 
-function toDAO(dto: Journey): JourneyDAO {
+export function toJourneyDAO(dto: Journey): JourneyDAO {
   return {
     actions_persistence: dto.lifeInsurance.actionsPersistence,
     arrival_date: dto.historicalPeriod.arrivalDate,
